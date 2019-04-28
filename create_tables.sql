43ab0d3af737940b7011d398cf9a7a8181a105ef
hscorrea <hscorrea@thoughtworks.com>
2019-04-28 19:46:50 -0300

    first

---
+++ create_tables.sql
@@ -0,0 +1,103 @@
+DROP TABLE IF EXISTS fato;
+DROP TABLE IF EXISTS bridge_keywords;
+DROP TABLE IF EXISTS bridge_cast;
+DROP TABLE IF EXISTS dim_movie;
+DROP TABLE IF EXISTS dim_cast;
+DROP TABLE IF EXISTS dim_actor;
+DROP TABLE IF EXISTS dim_year;
+DROP TABLE IF EXISTS dim_country;
+DROP TABLE IF EXISTS dim_genre;
+DROP TABLE IF EXISTS dim_director;
+DROP TABLE IF EXISTS dim_likes;
+DROP TABLE IF EXISTS dim_imdb;
+DROP TABLE IF EXISTS dim_keywords;
+DROP TABLE IF EXISTS dim_keyword;
+
+CREATE TABLE dim_movie (
+  movie_id SERIAL PRIMARY KEY,
+  movie_title VARCHAR UNIQUE,
+  duration NUMERIC,
+  color VARCHAR,
+  content_rating VARCHAR
+);
+
+CREATE TABLE dim_cast (
+  cast_id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE dim_actor (
+  actor_id SERIAL PRIMARY KEY,
+  facebook_likes INTEGER,
+  actor_name VARCHAR UNIQUE
+);
+
+CREATE TABLE bridge_cast (
+  cast_id INTEGER REFERENCES dim_cast(cast_id),
+  actor_id INTEGER REFERENCES dim_actor(actor_id)
+);
+
+CREATE TABLE dim_year (
+  year_id SERIAL PRIMARY KEY,
+  year_date INTEGER
+);
+
+CREATE TABLE dim_country (
+  country_id SERIAL PRIMARY KEY,
+  country_name VARCHAR UNIQUE,
+  language VARCHAR
+);
+
+CREATE TABLE dim_genre (
+  genre_id SERIAL PRIMARY KEY,
+  genre VARCHAR UNIQUE
+);
+
+CREATE TABLE dim_director (
+  director_id SERIAL PRIMARY KEY,
+  director_name VARCHAR UNIQUE,
+  director_facebook_likes INTEGER
+);
+
+CREATE TABLE dim_likes (
+  likes_id SERIAL PRIMARY KEY,
+  movie_facebook_likes INTEGER,
+  cast_total_likes INTEGER
+);
+
+CREATE TABLE dim_imdb (
+  imdb_id SERIAL PRIMARY KEY,
+  num_voted_users INTEGER,
+  num_user_for_review INTEGER,
+  num_critic_for_review INTEGER,
+  imdb_score INTEGER
+);
+
+CREATE TABLE dim_keywords (
+  keywords_id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE dim_keyword (
+  keyword_id SERIAL PRIMARY KEY,
+  keyword VARCHAR UNIQUE
+);
+
+CREATE TABLE bridge_keywords (
+  keywords_id INTEGER REFERENCES dim_keywords(keywords_id),
+  keyword_id INTEGER REFERENCES dim_keyword(keyword_id)
+);
+
+CREATE TABLE fato (
+  fato_id SERIAL PRIMARY KEY,
+  budget NUMERIC,
+  gross NUMERIC,
+  profit NUMERIC,
+  movie_id INTEGER REFERENCES dim_movie(movie_id),
+  cast_id INTEGER REFERENCES dim_cast(cast_id),
+  year_id INTEGER REFERENCES dim_year(year_id),
+  country_id INTEGER REFERENCES dim_country(country_id),
+  genre_id INTEGER REFERENCES dim_genre(genre_id),
+  likes_id INTEGER REFERENCES dim_likes(likes_id),
+  imdb_id INTEGER REFERENCES dim_imdb(imdb_id),
+  director_id INTEGER REFERENCES dim_director(director_id),
+  keywords_id INTEGER REFERENCES dim_keywords(keywords_id)
+);
---
+++ generate_inserts.rb
@@ -0,0 +1,64 @@
+require 'csv'    
+
+csv = CSV.parse(File.read('movies.csv'), headers: true)
+
+csv.each do |row|
+  columns = row.to_s.split(',')
+
+  row_number = columns[0].to_i
+  color = columns[1]
+  director_name = columns[2]
+  num_critic_for_reviews = columns[3]
+  duration = columns[4]
+  director_facebook_likes = columns[5]
+  actor_3_facebook_likes = columns[6]
+  actor_2_name = columns[7]
+  actor_1_facebook_likes = columns[8]
+  gross = columns[9]
+  genre = columns[10].split('|').first
+  actor_1_name = columns[11]
+  movie_title = columns[12]
+  num_voted_users = columns[13]
+  cast_total_facebook_likes = columns[14]
+  actor_3_name = columns[15]
+  plot_keywords = columns[16].split('|')
+  num_user_for_reviews = columns[17]
+  language = columns[18]
+  country = columns[19]
+  content_rating = columns[20]
+  budget = columns[21]
+  title_year = columns[22]
+  actor_2_facebook_likes = columns[23]
+  imdb_score = columns[24]
+  movie_facebook_likes = columns[25].split(' ').first
+
+  puts "INSERT INTO dim_movie VALUES (#{row_number}, '#{movie_title}', #{duration}, '#{color}', '#{content_rating}');"
+  puts "INSERT INTO dim_cast VALUES (#{row_number});"
+  puts "INSERT INTO dim_actor VALUES (#{row_number}, #{actor_1_facebook_likes}, '#{actor_1_name}');"
+  puts "INSERT INTO dim_actor VALUES (#{row_number + 1}, #{actor_2_facebook_likes}, '#{actor_2_name}');"
+  puts "INSERT INTO dim_actor VALUES (#{row_number + 2}, #{actor_3_facebook_likes}, '#{actor_3_name}');"
+  
+  # bridge_cast
+  puts "INSERT INTO bridge_cast VALUES (#{row_number}, #{row_number});"
+  puts "INSERT INTO bridge_cast VALUES (#{row_number}, #{row_number + 1});"
+  puts "INSERT INTO bridge_cast VALUES (#{row_number}, #{row_number + 2});"
+
+  puts "INSERT INTO dim_year VALUES (#{row_number}, #{title_year});"
+  puts "INSERT INTO dim_country VALUES (#{row_number}, '#{country}', '#{language}');"
+  puts "INSERT INTO dim_genre VALUES (#{row_number}, '#{genre}');"
+  puts "INSERT INTO dim_director VALUES (#{row_number}, '#{director_name}', #{director_facebook_likes});"
+  puts "INSERT INTO dim_likes VALUES (#{row_number}, #{movie_facebook_likes}, #{cast_total_facebook_likes});"
+  puts "INSERT INTO dim_imdb VALUES (#{row_number}, #{num_voted_users}, #{num_user_for_reviews}, #{num_critic_for_reviews}, #{imdb_score});"
+  puts "INSERT INTO dim_keywords VALUES (#{row_number});"
+
+  # inserindo cada keyword separadamente
+  plot_keywords.each_with_index do |keyword, index|
+    puts "INSERT INTO dim_keyword VALUES (#{row_number + index}, '#{keyword}');"
+    
+    # bridge_keywords
+    puts "INSERT INTO bridge_keywords VALUES (#{row_number}, #{row_number + index});"
+  end
+
+  # fato
+  puts "INSERT INTO fato VALUES (#{row_number}, #{budget}, #{gross}, #{gross.to_i - budget.to_i}, #{row_number}, #{row_number}, #{row_number}, #{row_number}, #{row_number}, #{row_number}, #{row_number}, #{row_number}, #{row_number});"
+end
